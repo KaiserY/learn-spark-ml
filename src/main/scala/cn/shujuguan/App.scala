e8691b7e88ef1a9a7633848c3332060070853aa8
yueyang <yueyang@yueyang.chart2.com>
2016-01-25 11:13:46 +0800

    mod

--- src/main/scala/cn/shujuguan/App.scala
+++ src/main/scala/cn/shujuguan/App.scala
@@ -2,12 +2,6 @@ package cn.shujuguan
 
 object App {
   def main(args: Array[String]) {
-    "11".getBytes("Unicode").foreach(b => println((b & 0xff)))
-
-    val countArray: Array[Int] = Array.fill(5) {
-      0
-    }
-
-    println(countArray.mkString(" "))
+    
   }
 }
